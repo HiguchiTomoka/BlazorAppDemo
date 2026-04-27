3718b40236afef3d6f48dc0de3a080343086ef9a
友香 樋口 <to-higuchi@fusion-com.jp>
2026-04-27 17:29:35 +0900

    変更

--- BlazorAppDemo/Components/Model/User.cs
+++ BlazorAppDemo/Components/Model/User.cs
@@ -2,10 +2,19 @@ namespace BlazorAppDemo.Components.Model
 {
     public class User
     {
+        // ID
         public int Id { get; set; }
+        // 名前
         public string? Name { get; set; }
+        // 年齢
         public int Age { get; set; }
+        // オンラインか
         public bool IsActive { get; set; }
+        // 性別
         public required string Gender { get; set; }
+        // 所属
+        public string? Affiliated { get; set; }
+        // 住所
+        public string? Address { get; set; }
     }
 }
